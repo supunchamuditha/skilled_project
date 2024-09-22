a61ea6de19a60d09b1b3ada3c96a42546a79906b
Pasith Senevirathna <pasith.senevi02@gmail.com>
2024-09-22 15:07:23 +0530

    Add authentication middleware with JWT verification and cookie parsing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRouter from "./routes/authRoutes.js";
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 //Test the API
 app.get("/", (req, res) => {
