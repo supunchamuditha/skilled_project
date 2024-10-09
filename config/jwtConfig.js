a8a78f460001cde68a4795148d02d4673871fc60
Pasith Senevirathna <pasith.senevi02@gmail.com>
2024-10-09 18:45:19 +0530

    Remove unused files and code

--- config/jwtConfig.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export default {
-    secret: process.env.JWT_SECRET,
-    expiresIn: process.env.JWT_EXPIRES_IN,
-};
--- middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error(err.message);
-  return res.status(500).json({ message: "Internal server error" });
-};
-
-export default errorHandler;
