3de9f95e578d2fd5807b0b9673536372d8921948
Pasith Senevirathna <pasith.senevi02@gmail.com>
2024-10-13 12:21:23 +0530

    Refactor authMiddleware.js to fix error handling and add user ID and user type to request object

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,7 +7,7 @@ const authMiddleware = (req, res, next) => {
     if (!token) {
       return res
         .status(401)
-        .jsom({ message: "Unauthorized - No token provided" });
+        .json({ message: "Unauthorized - No token provided" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -16,10 +16,12 @@ const authMiddleware = (req, res, next) => {
       return res.status(401).json({ message: "Unauthorized - Invalid token" });
     }
 
+    req.userId = decoded.id;
+    req.userType = decoded.userType;
     next();
   } catch (error) {
     console.error("Error in authMiddleware", error.message);
-    res.status(500).send({ message: "Internal server error" });
+    return res.status(500).send({ message: "Internal server error" });
   }
 };
 
