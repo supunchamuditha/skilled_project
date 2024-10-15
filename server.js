acfcf67d4bd7762801ce546dfb9e9ee08fcdc57b
Pasith Senevirathna <pasith.senevi02@gmail.com>
2024-10-16 00:44:27 +0530

    Add cors package for handling cross-origin resource sharing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRouter from "./routers/authRouter.js";
 import userRouter from "./routers/userRouter.js";
@@ -11,6 +12,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
