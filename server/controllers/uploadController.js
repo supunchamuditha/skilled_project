b9ea085426fa76895974283688796179dd982756
Pasith Senevirathna <pasith.senevi02@gmail.com>
2024-11-15 23:25:32 +0530

    Enhance file upload limits to allow a maximum file size of 8MB

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -1,7 +1,12 @@
 import multer from "multer";
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 8, // 8MB
+  },
+});
 
 export const uploadPPCV = upload.fields([
   { name: "profile_pic", maxCount: 1 },
