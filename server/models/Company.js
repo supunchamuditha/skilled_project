87ddd2a55dc48520626b3525026b9a7b37b6413a
Pasith Senevirathna <pasith.senevi02@gmail.com>
2024-11-18 19:44:48 +0530

    Refactor Company model to improve formatting and disable timestamps

--- server/models/Company.js
+++ server/models/Company.js
@@ -1,18 +1,24 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../config/db.js';
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
 
-const Company = sequelize.define('Company', {
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, unique: true, allowNull: false },
-  location: { type: DataTypes.STRING },
-  phone_num: { type: DataTypes.STRING },
-  industry: { type: DataTypes.STRING },
-  logo: { type: DataTypes.STRING },
-  logo_type: { type: DataTypes.STRING },
-  password: { type: DataTypes.STRING, allowNull: false },
-  isVerified: { type: DataTypes.STRING },
-  date: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
-  status: { type: DataTypes.INTEGER }
-});
+const Company = sequelize.define(
+  "Company",
+  {
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, unique: true, allowNull: false },
+    location: { type: DataTypes.STRING },
+    phone_num: { type: DataTypes.STRING },
+    industry: { type: DataTypes.STRING },
+    logo: { type: DataTypes.STRING },
+    logo_type: { type: DataTypes.STRING },
+    password: { type: DataTypes.STRING, allowNull: false },
+    isVerified: { type: DataTypes.STRING },
+    date: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    status: { type: DataTypes.INTEGER },
+  },
+  {
+    timestamps: false,
+  }
+);
 
 export default Company;
