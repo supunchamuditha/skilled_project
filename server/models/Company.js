e89848d342c110a5316064c223911d2d2814783c
Pasith Senevirathna <pasith.senevi02@gmail.com>
2024-11-18 19:43:48 +0530

    Refactor Company model to update database import path and add logo_type field

--- server/models/Company.js
+++ server/models/Company.js
@@ -1,5 +1,5 @@
 import { DataTypes } from 'sequelize';
-import sequelize from '../config/database.js';
+import sequelize from '../config/db.js';
 
 const Company = sequelize.define('Company', {
   name: { type: DataTypes.STRING, allowNull: false },
@@ -8,9 +8,8 @@ const Company = sequelize.define('Company', {
   phone_num: { type: DataTypes.STRING },
   industry: { type: DataTypes.STRING },
   logo: { type: DataTypes.STRING },
+  logo_type: { type: DataTypes.STRING },
   password: { type: DataTypes.STRING, allowNull: false },
-  verificationCode: { type: DataTypes.STRING },
-  verificationExpiration: { type: DataTypes.DATE },
   isVerified: { type: DataTypes.STRING },
   date: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
   status: { type: DataTypes.INTEGER }
